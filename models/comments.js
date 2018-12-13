7f24941053a7ed4ca7f19afac893130c8154b4a1
smattis18 <smattis@cfl.rr.com>
2018-12-13 18:45:51 -0500

    Updated models

--- models/comments.js
+++ models/comments.js
@@ -6,12 +6,10 @@ module.exports = function(sequelize, DataTypes) {
       },
       createdAt: {
         type: DataTypes.DATE,
-        field: 'beginTime',
         defaultValue: sequelize.literal('NOW()')
       },
       updatedAt: {
         type: DataTypes.DATE,
-        field: 'nextTime',
         defaultValue: sequelize.literal('NOW()')
       }
     });
