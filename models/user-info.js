d1f5a124e66fe1e2eb92b4221f044c8db2c021f3
smattis18 <smattis@cfl.rr.com>
2018-12-27 17:21:10 -0500

    Added auth html pages

---
+++ models/user-info.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      screen_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('NOW()')
+      }
+    });
+  
+    User.associate = function(models) {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      User.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return User;
+  };
